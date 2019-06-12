f2b4e9a6ddbb574ed713dcbd20519b6a4554df70
Cai Yun Ting <caiyuntingcfrc@gmail.com>
2019-06-13 00:02:44 +0800

    0612

--- BMI.R
+++ BMI.R
@@ -4,7 +4,7 @@
 # rm
 rm(list = ls())
 # set working directory
-setwd("D:/R_wd/")
+setwd("~/R_wd/twin_cities/")
 # list of packages
 list.packages <- c("tidyverse", "magrittr", "haven", 
                    "gridExtra", "summarytools", "tabulizer", 
@@ -26,7 +26,7 @@ st_options(style = "simple",
 
 # read sav file -----------------------------------------------------------
 
-filepath <- "Twin Cities/3.雙城正式_全年級20180704final_han&Lung.sav"
+filepath <- "3.雙城正式_全年級20180704final_han&Lung.sav"
 df <- read_sav(filepath, encoding = "UTF-8", user_na = FALSE)
 var_lables <- sapply(df, attr, "label")
 vlu_lables <- sapply(df, attr, "labels")
@@ -36,7 +36,7 @@ rm(filepath)
 # extract the BMI table ---------------------------------------------------
 
 # read the pdf file
-filepath <- "Twin Cities/BMI/衛福部_2013_兒童與青少年生長身體質量指數(BMI)建議值.pdf"
+filepath <- "~/Downloads/衛福部_2013_兒童與青少年生長身體質量指數(BMI)建議值.pdf"
 tbl_BMI <- extract_tables(file = filepath, encoding = "UTF-8") %>% 
         .[[1]] %>% 
         .[-(1:4), ]
@@ -63,42 +63,51 @@ names(tbl_BMI) <- c("age", "m_low", "m_upp", "m_ovr",
 
 
 # calculate BMIs ----------------------------------------------------------
-# male children
 
 df <- df %>% mutate(BMI = w_kg / ((h_cm / 100) ** 2), 
-                    ovrw = 0)
+                    ovrw = NA)
 
-for(i in 2:6) {
-        i <- 6
-        m <- df$realage < tbl_BMI$age[i] & df$realage >= tbl_BMI$age[i - 1] & df$Bd23 == 2
-        a <- df[m, ] %>%
-                mutate(., ovrw = case_when(BMI < tbl_BMI$m_low[i - 1] ~ 1,
-                                           BMI > tbl_BMI$m_low[i - 1] & BMI < tbl_BMI$m_upp[i - 1] ~ 2, 
-                                           BMI >= tbl_BMI$m_upp[i - 1] & BMI < tbl_BMI$m_ovr[i - 1] ~ 3, 
-                                           BMI >= tbl_BMI$m_ovr[i - 1] ~ 4)
-                )
-        df[a$SID, ]$ovrw <- a$ovrw
+
+# for loop ----------------------------------------------------------------
+
+for(i in 2:nrow(tbl_BMI)) {
+        # male children
+        for(j in 1:nrow(df)) {
+                b <- !is.na(df$BMI[j]) & !is.na(df$realage[j]) & !is.na(df$Bd23[j])
+                # sex == 2
+                s <- df$Bd23[j] == 2
+                # age
+                m <- df$realage[j] < tbl_BMI$age[i] & df$realage[j] >= tbl_BMI$age[i - 1]
+                # below average
+                c1 <- df$BMI[j] < tbl_BMI$m_low[i - 1]
+                # normal
+                c2 <- df$BMI[j] > tbl_BMI$m_low[i - 1] & df$BMI[j] < tbl_BMI$m_upp[i - 1]
+                # overweight
+                c3 <- df$BMI[j] >= tbl_BMI$m_upp[i - 1] & df$BMI[j] < tbl_BMI$m_ovr[i - 1]
+                # more than overweight
+                c4 <- df$BMI[j] >= tbl_BMI$m_ovr[i - 1]
+                if(b & s & m & c1) { df$ovrw[j] <- 1 } 
+                if(b & s & m & c2) { df$ovrw[j] <- 2 } 
+                if(b & s & m & c3) { df$ovrw[j] <- 3 } 
+                if(b & s & m & c4) { df$ovrw[j] <- 4 } 
+                }
+        # female children
+        for(j in 1:nrow(df)) {
+                b <- !is.na(df$BMI[j]) & !is.na(df$realage[j]) & !is.na(df$Bd23[j])
+                s <- df$Bd23[j] == 1
+                f <- df$realage[j] < tbl_BMI$age[i] & df$realage[j] >= tbl_BMI$age[i - 1]
+                c1 <- df$BMI[j] < tbl_BMI$f_low[i - 1]
+                c2 <- df$BMI[j] > tbl_BMI$f_low[i - 1] & df$BMI[j] < tbl_BMI$f_upp[i - 1]
+                c3 <- df$BMI[j] >= tbl_BMI$f_upp[i - 1] & df$BMI[j] < tbl_BMI$f_ovr[i - 1]
+                c4 <- df$BMI[j] >= tbl_BMI$f_ovr[i - 1]
+                if(b & s & f & c1) { df$ovrw[j] <- 1 } 
+                if(b & s & f & c2) { df$ovrw[j] <- 2 } 
+                if(b & s & f & c3) { df$ovrw[j] <- 3 } 
+                if(b & s & f & c4) { df$ovrw[j] <- 4 } 
+                }
         }
-        
-        
-        f <- df$realage < tbl_BMI$age[i] & df$realage >= tbl_BMI$age[i - 1] & df$Bd23 == 1 
-        b <- filter(df, f) %>% 
-                mutate(., ovrw = case_when(BMI < tbl_BMI$f_low[i - 1] ~ 1,
-                                        BMI > tbl_BMI$f_low[i - 1] & BMI < tbl_BMI$f_upp[i - 1] ~ 2, 
-                                        BMI >= tbl_BMI$f_upp[i - 1] & BMI < tbl_BMI$f_ovr[i - 1] ~ 3, 
-                                        BMI >= tbl_BMI$f_ovr[i - 1] ~ 4)
-                       )
-        df[b$SID, 284] <- b$ovrw
-        }
 
 
-
-m <- df %>% 
-        filter(realage < tbl_BMI$age[14] & realage >= tbl_BMI$age[13]) %>% 
-        filter(Bd23 == 2) %>% 
-        mutate(ovrw = case_when(BMI < tbl_BMI$m_low[13] ~ 1,
-                                BMI > tbl_BMI$m_low[13] & BMI < tbl_BMI$m_upp[13] ~ 2, 
-                                BMI >= tbl_BMI$m_upp[13] & BMI < tbl_BMI$m_ovr[13] ~ 3, 
-                                BMI >= tbl_BMI$m_ovr[13] ~ 4)
-        )
-df$ovrw[df$SID %in% m$SID] <- m$ovrw
+# save file ---------------------------------------------------------------
+write_sav(df, "~/Desktop/tp_all.sav", compress = FALSE)
+read_sav("~/Desktop/tp_all.sav", encoding = "UTF-8")
