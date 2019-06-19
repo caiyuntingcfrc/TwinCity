765e994b82c3e1d34ab760de0126c3598a815ffc
CAI, YUN-TING <caiyuntingcfrc@gmail.com>
2019-06-19 08:53:16 +0800

    0619

--- BMI_SF.R
+++ BMI_SF.R
@@ -5,15 +5,12 @@
 rm(list = ls())
 # set working directory
 setwd("D:/R_wd/Twin Cities/")
-library(devtools)
-# devtools::install_version("sjPlot", version = "1.7")
-# install.packages("sjPlot", lib = "c:/Program Files/R/library/")
 
 # list of packages
 list.packages <- c("tidyverse", "magrittr", "haven", 
                    "gridExtra", "summarytools", "tabulizer", 
                    "labelled", "DescTools", "userfriendlyscience", "stargazer", 
-                   "sjlabelled", "sjmisc", "sjstats", "ggeffects")
+                   "sjlabelled", "sjmisc", "sjstats", "ggeffects", "sjPlot")
 # check if the packages are installed
 new.packages <- list.packages[!(list.packages %in% installed.packages()[ , "Package"])]
 # install new packages
@@ -22,49 +19,142 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(list.packages, require, character.only = TRUE)
 # remove lists
 rm(list.packages, new.packages)
-# 
-library(sjPlot, lib.loc = "c:/Program Files/R/myLibrary/")
 # options
-options(scipen = 999, 
-        autoSetValueLabels = TRUE, 
-        autoSetVariableLabels = TRUE)
-# options for summarytools
-st_options(style = "simple",
-           round.digits = 4,
-           ctable.prop = "c")
+options(scipen = 999)
 
 # Read the Data File ------------------------------------------------------
 
-# read .sav file
 filepath <- "11. Taipei_all.sav"
-df <- read_sav(filepath, encoding = "UTF-8")
+df <- read_sav(filepath)
 
 
 # bmi z-score -------------------------------------------------------------
 
 
 
-# Desc --------------------------------------------------------------------
-t <- table(BMI = df$bmicat, 
-           Grade = df$grade, df$Bd23)
 
-sjt.xtab(var.row = df$bmicat, var.col = df$grade,  
-         var.grp = df$Bd23, 
-         variableLabels = c("BMI 分類", 
-                            "年級", 
-                            "性別"),
-         stringTotal = "總和",
-         showHorizontalLine = TRUE,
-         showCellPerc = TRUE,
-         showLegend = FALSE,
-         showSummary = FALSE,
-         highlightTotal = TRUE,
-         encoding = "CP950")
+# Desc (BMI) --------------------------------------------------------------
 
-# detach sjPlot 1.7
-detach("package:sjPlot", unload = TRUE)
-# load sjPlot 2.3
-library(sjPlot, lib.loc = "c:/Program Files/R/library")
+# ctable BMI x grade
+sjt.xtab(var.row = df$bmicat, var.col = df$grade, 
+         var.labels = c("BMI分類", "年級"), 
+         title = "BMI分類 X 年級", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8", 
+         file= "BMIxGrade.doc")
+# plot BMI x grade
+sjp.xtab(x = df$grade, grp = df$bmicat, 
+         title = "BMI分類 X 年級", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot grade x BMI
+sjp.xtab(x = df$bmicat, grp = df$grade, 
+         title = "年級 X BMI分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
 
-# plot
-sjp.grpfrq(df$bmicat, df$Bd23, show.legend = TRUE)
+# ctable BMI X Sex
+sjt.xtab(var.row = df$bmicat, var.col = df$Bd23, 
+         var.labels = c("BMI分類", "性別"), 
+         title = "BMI分類 X 性別", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8",
+         file = "BMIxSex.doc")
+# plot BMI X Sex
+sjp.xtab(x = df$Bd23, grp = df$bmicat, 
+         title = "BMI分類 X 性別", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot Sex X BMI
+sjp.xtab(x = df$bmicat, grp = df$Bd23, 
+         title = "性別 X BMI分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+# ctable BMI X SF
+sjt.xtab(var.row = df$bmicat, var.col = df$Sf, 
+         var.labels = c("BMI分類", "家庭組織型態"), 
+         title = "BMI分類 X 家庭組織型態", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8", 
+         file = "BMIxSf.doc")
+# plot BMI X Sf
+sjp.xtab(x = df$Sf, grp = df$bmicat, 
+         title = "BMI分類 X 家庭組織型態", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot Sf X BMI
+sjp.xtab(x = df$bmicat, grp = df$Sf, 
+         title = "家庭組織型態 X BMI分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+
+# Desc (illness) ----------------------------------------------------------
+
+
+# ctable illness X grade
+sjt.xtab(var.row = df$illness, var.col = df$grade, 
+         var.labels = c("身心狀況分類", "年級"), 
+         title = "身心狀況分類 X 年級", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8", 
+         file = "illnessxGrade.doc")
+# plot illness X grade
+sjp.xtab(x = df$grade, grp = df$illness, 
+         title = "身心狀況分類 X 年級", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot grade X illness
+sjp.xtab(x = df$illness, grp = df$grade, 
+         title = "年級 X 身心狀況分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+# ctable illness X sex
+sjt.xtab(var.row = df$illness, var.col = df$Bd23, 
+         var.labels = c("身心狀況分類", "性別"), 
+         title = "身心狀況分類 X 性別", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8", 
+         file = "illnessxSex.doc")
+# plot illness X Sex
+sjp.xtab(x = df$Bd23, grp = df$illness, 
+         title = "身心狀況分類 X 性別", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot Sex X illness
+sjp.xtab(x = df$illness, grp = df$Bd23, 
+         title = "性別 X 身心狀況分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+# ctable illness X Sf
+sjt.xtab(var.row = df$illness, var.col = df$Sf, 
+         var.labels = c("身心狀況分類", "家庭組織型態"), 
+         title = "身心狀況分類 X 家庭組織型態", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = FALSE, show.col.prc = FALSE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8", 
+         file = "illnessxSf.doc")
+# plot illness X Sf
+sjp.xtab(x = df$Sf, grp = df$illness, 
+         title = "身心狀況分類 X 家庭組織型態", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+# plot Sex X illness
+sjp.xtab(x = df$illness, grp = df$Sf, 
+         title = "家庭組織型態 X 身心狀況分類", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
--- Twin_illness.R
+++ Twin_illness.R
@@ -4,12 +4,13 @@
 # rm
 rm(list = ls())
 # set working directory
-setwd("D:/R_wd/Twin Cities/")
+setwd("D:/R_wd/")
+
 # list of packages
-list.packages <- c("tidyverse", "magrittr", "haven", 
+list.packages <- c("tidyverse", "magrittr", "haven", "readxl",
                    "gridExtra", "summarytools", "tabulizer", 
-                   "labelled", "DescTools", "userfriendlyscience", 
-                   "stargazer", "sjPlot")
+                   "labelled", "DescTools", "userfriendlyscience", "stargazer", 
+                   "sjlabelled", "sjmisc", "sjstats", "ggeffects", "sjPlot")
 # check if the packages are installed
 new.packages <- list.packages[!(list.packages %in% installed.packages()[ , "Package"])]
 # install new packages
@@ -20,10 +21,6 @@ lapply(list.packages, require, character.only = TRUE)
 rm(list.packages, new.packages)
 # options
 options(scipen = 999)
-# options for summarytools
-st_options(style = "simple",
-           round.digits = 4,
-           ctable.prop = "c")
 
 # import excel file -------------------------------------------------------
 
@@ -34,7 +31,7 @@ colnames(re) <- c("health_related", "illness", "major_illness", "others")
 
 # read sav file -----------------------------------------------------------
 
-sav_path <- "Twin Cities/Taipei_all.sav"
+sav_path <- "Twin Cities/11. Taipei_all.sav"
 df <- read_sav(sav_path, encoding = "UTF-8")
 
 
@@ -44,20 +41,58 @@ df <- df %>% mutate(illness = case_when(Ch3 == 1 & Ch3說明 %in% re$health_rela
                                         Ch3 == 1 & Ch3說明 %in% re$illness ~ 2,
                                         Ch3 == 1 & Ch3說明 %in% re$major_illness ~ 3, 
                                         Ch3 == 1 & Ch3說明 %in% re$others ~ 4, 
-                                        TRUE ~ 99))
+                                        is.na(Ch3) | is.na(Ch3說明) ~ NA_real_))
 # attributes
-attr(df$illness, "label") <- "身心狀況分類"
-attr(df$illness, "labels") <- c(1:4, 99)
-names(attr(df$illness, "labels")) <- c("健康問題", "疾病", "重大疾病", "無法歸類")
+df$illness <- labelled(df$illness, c("健康問題" = 1, 
+                                     "疾病" = 2, 
+                                     "重大疾病" = 3, 
+                                     "無法歸類" = 4), 
+                       label = "身心狀況分類")
+attr(df$illness, "format.spss") <- "F8.2"
 
-# frequencies -------------------------------------------------------------
 
-# illness X ovrw
+# save the file -----------------------------------------------------------
+
+write_sav(df, "Twin Cities/Taipei_all.sav")
+
+# Desc --------------------------------------------------------------------
+
+# illness X grade
+sjt.xtab(var.row = df$illness, var.col = df$grade, 
+         var.labels = c("身心狀況分類", "年級"), 
+         title = "身心狀況分類 X 年級", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = TRUE, show.col.prc = TRUE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8")
+sjp.xtab(x = df$grade, grp = df$illness, 
+         title = "身心狀況分類 X 年級", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+# illness X sex
+sjt.xtab(var.row = df$illness, var.col = df$Bd23, 
+         var.labels = c("身心狀況分類", "性別"), 
+         title = "身心狀況分類 X 性別", 
+         string.total = "總和", 
+         show.cell.prc = TRUE, show.row.prc = TRUE, show.col.prc = TRUE,
+         show.summary = FALSE, show.legend = TRUE, 
+         emph.total = TRUE,
+         digits = 2,
+         encoding = "UTF-8")
+sjp.xtab(x = df$illness, grp = df$Bd23, 
+         title = "身心狀況分類 X 性別", 
+         show.total = TRUE, show.n = TRUE, show.prc = TRUE) + theme_sjplot()
+
+
+# illness X bmicat
 sjt.xtab(df$illness, 
-         df$ovrw, 
+         df$bmicat, 
          encoding = "UTF-8", digits = 2, 
          title = "雙城大台北地區 — 身心狀況分類 X BMI標準", 
-         show.cell.prc = TRUE, show.legend = TRUE, 
+         show.cell.prc = TRUE, show.row.prc = TRUE, show.col.prc = TRUE,
+         show.legend = TRUE, 
          string.total = "總和", emph.total = TRUE, show.na = FALSE)
 
 # test --------------------------------------------------------------------
