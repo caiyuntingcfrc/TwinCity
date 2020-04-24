a1e740f6af56d9dd893b8e406a93cfc9d25b1ee2
CAI, YUN-TING <caiyuntingcfrc@gmail.com>
2020-04-24 17:36:29 +0800

    0424

--- socio_rev.R
+++ socio_rev.R
@@ -23,20 +23,68 @@ d <- df %>%
         as.data.frame()
 # desc
 desc <- stat.desc(d, desc = TRUE) %>% 
-        round(2)
+        round(2) %>% 
+        .[-7, ]
+
+# plots-vanilla -----------------------------------------------------------
+
 # boxplot
 boxplot(d)
 
-qplot(d$Ch4_attention_sum, binwidth = .5) + sjPlot::theme_sjplot2()
-qplot(d$Ch4_soc_sum, binwidth = .5) + sjPlot::theme_sjplot2()
+# bar plot
+p1 <- ggplot(d, aes(x = Ch4_aggressive_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p2 <- ggplot(d, aes(x = Ch4_attention_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p3 <- ggplot(d, aes(x = Ch4_soc_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p4 <- ggplot(d, aes(x = Ch4_withdrawal_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2)
 
 # desc-filter -------------------------------------------------------------
-# filter 
+# filter Ft9, Bd21
+# n: 5666
 d <- df %>% 
         # caregiver: parents(1), the father(2) and the mother(3)
         filter(Ft9 %in% c(1, 2, 3)) %>% 
         # respondent: the mother(1) and the father(2)
         filter(Bd21 %in% c(1, 2))
-str(df)
-boxplot()
+# select Ch4_
+d1 <- d %>% 
+        select(matches("^Ch4_.*_sum")) %>% 
+        as.data.frame()
+# desc
+desc <- stat.desc(d1, desc = TRUE) %>% 
+        round(2) %>% 
+        .[-7, ]
+# boxplot
+boxplot(d1)
+
+# bar plot
+p1 <- ggplot(d1, aes(x = Ch4_aggressive_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p2 <- ggplot(d1, aes(x = Ch4_attention_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p3 <- ggplot(d1, aes(x = Ch4_soc_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+p4 <- ggplot(d1, aes(x = Ch4_withdrawal_sum)) + 
+        geom_bar() + 
+        theme_pubclean()
+
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2)
 
